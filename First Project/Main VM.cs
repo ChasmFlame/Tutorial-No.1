a1e0db67e71f81a2848c10d9fea05da593e44ae8
Ben McQueen <Ben.McQueen@Crofton.Consulting>
2020-04-21 11:37:51 +0100

    Add project files.

---
+++ First Project/Main VM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace First_Project
+{
+    class Main_VM
+    {
+        private string DisplayText;
+        private int number;
+
+
+        public int Number
+        {
+            get
+			{
+				return number;
+			}
+            set => number = value;
+        }
+
+        public int IncrementNumber ()
+        {
+            return ++Number;   
+        }
+    }
+}
---
+++ First Project/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace First_Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Main_VM Mainvm =new Main_VM();
+
+        public MainWindow()
+        {
+			DataContext = Mainvm;
+            InitializeComponent();
+        }
+
+		private void Button1_Click(object sender, RoutedEventArgs e)
+		{
+			Mainvm.Number++;
+			InvalidateVisual();
+		}
+    }
+}
