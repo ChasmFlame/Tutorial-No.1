83af6a1adaf6cd11f2739270a3a7af634736e780
JasonSharp72 <alfhere.js@googlemail.com>
2020-04-28 15:45:46 +0100

    added basic graphics

---
+++ Agent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Space_Game
+{
+    public class Agent
+    {
+        int HitPoints, CurrentHitpoints;
+        int ShootingSkillLevel;
+        float X, Y;
+        public Agent()
+        {
+            HitPoints = 25;
+            CurrentHitpoints = HitPoints;
+            ShootingSkillLevel = 50;
+        }
+
+        public bool UseWeapon()
+        {
+            int [] Result = DiceController.Roll(1, 100);
+            return Result[0] < ShootingSkillLevel;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Space_Game
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		MainVM vm = new MainVM();
+		public MainWindow()
+		{
+			InitializeComponent();
+			DataContext = vm;
+
+		
+		}
+
+		private void Button_Shoot_Click(object sender, RoutedEventArgs e)
+		{
+			vm.ShootWeapon();
+		}
+
+		private void Window_KeyDown(object sender, KeyEventArgs e)
+		{
+			//vm.KeyPressed();
+		}
+
+		private void DataGrid_SourceUpdated(object sender, DataTransferEventArgs e)
+		{
+			DataGrid textLines = (DataGrid)sender;
+
+			if(textLines.Items.Count > 0)
+			{
+				var border = VisualTreeHelper.GetChild(textLines, 0) as Decorator;
+				if(border != null)
+				{
+					var scroll = border.Child as ScrollViewer;
+					if(scroll != null) scroll.ScrollToEnd();
+				}
+			}
+		}
+	}
+}
